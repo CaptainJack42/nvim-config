ca0e93e1951820d4f13235eedcaaa1d7540254b4
Janek <janek@rp-engineering.de>
2023-03-23 15:07:25 +0100

    increase scrolloff to 15

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,4 @@ vim.g.vimtex_view_method = "zathura"
 vim.g.latex_view_general_viewer = "zathura"
 vim.g.vimtex_compiler_progname = "nvr"
 vim.g.vimtex_quickfix_open_on_waring = 0
+vim.opt.scrolloff = 15
